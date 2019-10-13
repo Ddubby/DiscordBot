e7890d639f94b7de2ceca2990d398150059a3302
League Student <league@iMac-23.local>
2019-10-12 17:27:25 -0700

    Need to improve ChatBot w/ switch

--- src/main/java/org/jointheleague/modules/ChatMessageListener.java
+++ src/main/java/org/jointheleague/modules/ChatMessageListener.java
@@ -3,16 +3,18 @@ package org.jointheleague.modules;
 import java.util.ArrayList;
 
 import org.javacord.api.event.message.MessageCreateEvent;
+import org.w3c.dom.events.Event;
 
 public class ChatMessageListener extends CustomMessageCreateListener{
 
 	private int karma = 0;
-	private ArrayList<String> questions = new ArrayList<String>();
+	private boolean cursed = false;
+	private String finalS;
+	private ArrayList<String> gQuestions = new ArrayList<String>();
+	private ArrayList<String> bQuestions = new ArrayList<String>();
 	private ArrayList<String> gAnswers = new ArrayList<String>();
 	private ArrayList<String> bAnswers = new ArrayList<String>();
-	
-	
-	
+	private ArrayList<String> curseWords = new ArrayList<String>();
 	public ChatMessageListener(String channelName) {
 		super(channelName);
 		// TODO Auto-generated constructor stub
@@ -21,34 +23,57 @@ public class ChatMessageListener extends CustomMessageCreateListener{
 	@Override
 	public void handle(MessageCreateEvent event) {
 		// TODO Auto-generated method stub
-		questions.add("Hello");
-		questions.add("Hi");
-		questions.add("How was your day?");
-		gAnswers.add("Hello " + event.getMessageAuthor().getDisplayName() + " !");
+		gQuestions.add("Hello");
+		gQuestions.add("Hi");
+		gQuestions.add("How was your day?");
+		gAnswers.add("Hello " + event.getMessage().getAuthor().getDisplayName() + "!");
 		bAnswers.add("Hey there...");
-		gAnswers.add("Hi " + event.getMessageAuthor().getDisplayName() + " !");
+		gAnswers.add("Hi " + event.getMessage().getAuthor().getDisplayName() + "!");
 		bAnswers.add("Hey there...");
 		gAnswers.add("Good! How about you?");
+		bAnswers.add("Not good");
+		curseWords.add("test");
 		String message = event.getMessageContent().trim();
 		if (message.startsWith("//")) {
 			message.equals(message.substring(2));
-			event.getChannel().sendMessage(answer(message));	
+			finalS = answer(message);
+			event.getChannel().sendMessage(finalS);
+			if (cursed) {
+				//event.getMessage().edit("[DELETED BY CHATBOT]");
+				cursed = false;
+			}
 		}
 	}
 
 	public String answer(String message) {
-		
-		for (int i = 0; i < questions.size(); i++) {
-			if (message.contains(questions.get(i))) {
-				if (karma >= 0) {
-					message = gAnswers.get(i);
-					return message;
-				} else {
-					message = bAnswers.get(i);
-					return message;
-				}
+		for (String curse : curseWords) {
+			if (message.contains(curse)) {
+				cursed = true;
+				karma--;
+				message = "";
+				return "Don't curse! :angry:";
 			}
 		}
-		return "";
+		for (int i = 0; i < gQuestions.size(); i++) {
+			if (message.contains(gQuestions.get(i))) {
+				if (karma >= 0) {
+					karma++;
+					return gAnswers.get(i);
+				} else {
+					karma++;
+					return bAnswers.get(i);
+				}
+			}/* else if(message.contains(bQuestions.get(i))){
+				if (karma >= 0) {
+					message = bAnswers.get(i);
+					karma--;
+					return message;
+				} else {
+					karma--;
+					return "";
+				}
+			}*/
+		}
+		return "Sorry, but it seems like I cannot generate a logical response\nMost likely scenario, you typed nonsense";
 	}
 }
